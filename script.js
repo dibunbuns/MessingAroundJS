5160f05479cd392f8edeca63eb9f748ff93880a3
dibunbuns <bunniequeenrules@gmail.com>
2023-10-09 10:20:23 -0400

    Done!

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const startButton = document.getElementById("start");
 startButton.addEventListener("click", function () {
-  let userName = prompt("Who's there?");
+  let userName = prompt("Who's there?", " ");
   if (userName === "Admin") {
-    prompt("Enter Password");
-  } else if (userName === "cancel") {
-    prompt("Canceled");
-  } else prompt("I don't know who you are. Goodbye.");
+    let password = prompt("Enter Password");
+    if (password === "password") {
+      alert("Welcome Master");
+    } else if (password === null) {
+      alert("Terminated");
+    } else {
+      alert("That is incorrect fool. Goodbye.");
+    }
+  } else if (userName === null) {
+    alert("Canceled");
+  } else {
+    alert("I don't know who you are. Goodbye.");
+  }
 });
